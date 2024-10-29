7ae8ea2d30f127c0f481dc72f57dc510d1e9997c
herreang000 <herreang000@warren.k12.in.us>
2024-10-29 11:06:40 -0400

    final

--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,87 @@
-package PACKAGE_NAME;public class Student {
-}
+public class Student {
+    private String studentId;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String major;
+
+
+
+
+    public Student() {
+        studentId = "";
+        firstName = "";
+        lastName = "";
+        age = 0;
+        major = "";
+    }
+
+
+    public Student(String studentId, String firstName, String lastName, int age, String major) {// MORE FIELDS ) {
+        this.studentId = studentId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.major = major;
+    }
+
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+
+    //TODO: getters and setters
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+
+    public String getLastName() {
+        return lastName;
+    }
+
+
+    public void setLastName(String  lastName) {
+        this.lastName = lastName;
+    }
+
+
+    public int getAge() {
+        return age;
+    }
+
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+    public String getMajor() {
+        return major;
+    }
+
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+
+    @Override
+    public String toString() {
+        //TODO
+        return "Student ID: " + studentId + ", Name: " + firstName + " " +lastName + ", Age: " + age + ", Major: " + major;
+
+    }}
--- src/StudentRecord.java
+++ src/StudentRecord.java
@@ -1,2 +1,94 @@
-package PACKAGE_NAME;public class StudentRecord {
-}
+public class StudentRecord {
+
+    private String studentId1;
+    private String firstName1;
+    private String lastName1;
+    private int age1;
+    private String major1;
+
+
+
+
+    public StudentRecord() {
+        studentId1 = "";
+        firstName1 = "";
+        lastName1 = "";
+        age1 = 0;
+        major1 = "";
+    }
+
+
+
+    public record PersonRecord(String StudentId1, String firstName1,  String lastName1, int age1, String major1) {// MORE FIELDS ) {
+        this.studentId1 = studentId1;
+        this.firstName1 = firstName1;
+        this.lastName1 = lastName1;
+        this.age1 = age1;
+        this.major1 = major1;
+    }
+
+    public String getStudentId1() {
+        return studentId1;
+    }
+
+
+    public void setStudentId1(String studentId1) {
+        this.studentId1 = studentId1;
+    }
+
+
+    //TODO: getters and setters
+
+
+    public String getFirstName1() {
+        return firstName1;
+    }
+
+
+    public void setFirstName1(String firstName1) {
+        this.firstName1 = firstName1;
+    }
+
+
+    public String getLastName1() {
+        return lastName1;
+    }
+
+
+    public void setLastName1(String  lastName1) {
+        this.lastName1 = lastName1;
+    }
+
+
+    public int getAge1() {
+        return age1;
+    }
+
+
+    public void setAge1(int age1) {
+        this.age1 = age1;
+    }
+
+
+    public String getMajor1() {
+        return major1;
+    }
+
+
+    public void setMajor1(String major1) {
+        this.major1 = major1;
+    }
+
+
+    @Override
+    public String toString() {
+        //TODO
+        return "Student ID: " + studentId1 + ", Name: " + firstName1 + " " +lastName1 + ", Age: " + age1 + ", Major: " + major1;
+
+    }}
+
+
+
+
+
+
